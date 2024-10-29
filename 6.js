f6ad1c88b0cf23ddc2431cfa0fd00f1e1f764719
danula-ded <stupishin1597@yandex.ru>
2024-10-29 14:44:26 +0300

    add

--- 6.js
+++ 6.js
@@ -1,28 +1,28 @@
 async function* checkServerStatus() {
     while (true) {
-
-        const response = await fetch('https://jsonplaceholder.typicode.com/todos/');
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
         const data = await response.json();
-
-        if (data.completed) {
-            yield "OK";
-            break;
-        } else {
-            yield "Not OK";
+        console.log(data);
+        const status = Object.keys(data).length !== 0 ? 'OK' : 'NOT_OK';
+        if (status === 'OK') {
+            yield data.completed ? '200' : '404';
         }
-
+        else if (status === 'NOT_OK') {
+            yield status;
+        }
         await new Promise(resolve => setTimeout(resolve, 1000));
     }
 }
 
-(async () => {
-    const statusChecker = checkServerStatus();
-
-    for await (const status of statusChecker) {
-        console.log(`Статус сервера: ${status}`);
-        if (status === "OK") {
-            console.log("Сервер в порядке. Завершение проверки.");
+(async function startGenerator() {
+    const generator = checkServerStatus();
+    for await (const status of generator) {
+        console.log(status);
+        if (status === 'OK') {
+            break;
+        }
+        if (status === 'NOT_OK') {
             break;
         }
     }
-})();
+})()
