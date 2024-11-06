d5553c2f104d9c2026cf61c8f8f020b49a121ae0
Maksim <74344870+Rubsun@users.noreply.github.com>
2024-11-06 13:12:13 +0300

    Update weather_parser.js

--- weather_parser.js
+++ weather_parser.js
@@ -1,4 +1,4 @@
-const WEATHER_API_KEY = '9a92d0499ea244c691e101936243110';
+const WEATHER_API_KEY = 'your_api_key';
 const COUNTRY = 'Indonesia';
 const BASE_WEATHER_API_URL = 'http://api.weatherapi.com/v1/history.json';
 
@@ -8,10 +8,10 @@ const currentMonth = today.getMonth() + 1;
 const currentYear = today.getFullYear();
 
 const DT_MONTHS = [];
-const daysInMonth = (year, month) => new Date(year, month, 0).getDate();
+const days_in_month = (year, month) => new Date(year, month, 0).getDate();
 
 for (let i = 1; i <= 12; i++) {
-    let monthDays = daysInMonth(currentYear, i);
+    let monthDays = days_in_month(currentYear, i);
     for (let j = 1; j <= monthDays; j++) {
         let month = i < 10 ? `0${i}` : `${i}`;
         let day = j < 10 ? `0${j}` : `${j}`;
@@ -22,7 +22,7 @@ for (let i = 1; i <= 12; i++) {
 
 const DAYS = {};
 
-async function fetchWeatherData() {
+async function fetch_weather_data() {
     const weatherPromises = DT_MONTHS.map(dt => {
         const WEATHER_API_URL = `${BASE_WEATHER_API_URL}?key=${WEATHER_API_KEY}&q=${COUNTRY}&dt=${dt}`;
         return fetch(WEATHER_API_URL)
@@ -38,21 +38,20 @@ async function fetchWeatherData() {
             .catch(error => console.error(`Ошибка при получении данных для ${dt}:`, error));
     });
 
-    // Ждем выполнения всех запросов
     await Promise.all(weatherPromises);
-    calculateMonthlyStats();
+    calculate_monthly_stats();
 }
 
-function calculateMonthlyStats() {
+function calculate_monthly_stats() {
     let MONTHS_STATS = Array(12).fill().map(() => [0, 0, 0, {}]);
 
     for (let dt in DAYS) {
         const dateParts = dt.split('-');
         const monthIndex = parseInt(dateParts[1]) - 1;
 
-        MONTHS_STATS[monthIndex][0] += DAYS[dt][0];  // Сумма температур
-        MONTHS_STATS[monthIndex][1] += DAYS[dt][1];  // Сумма влажности
-        MONTHS_STATS[monthIndex][2] += 1;  // Количество дней
+        MONTHS_STATS[monthIndex][0] += DAYS[dt][0];
+        MONTHS_STATS[monthIndex][1] += DAYS[dt][1];
+        MONTHS_STATS[monthIndex][2] += 1;
 
         const condition = DAYS[dt][2];
         if (!MONTHS_STATS[monthIndex][3][condition]) {
@@ -63,8 +62,8 @@ function calculateMonthlyStats() {
 
     for (let i = 0; i < MONTHS_STATS.length; i++) {
         if (MONTHS_STATS[i][2] > 0) {
-            MONTHS_STATS[i][0] /= MONTHS_STATS[i][2];  // Средняя температура
-            MONTHS_STATS[i][1] /= MONTHS_STATS[i][2];  // Средняя влажность
+            MONTHS_STATS[i][0] /= MONTHS_STATS[i][2];
+            MONTHS_STATS[i][1] /= MONTHS_STATS[i][2];
 
             let mostOftenCondition = '';
             let maxCount = 0;
@@ -74,14 +73,14 @@ function calculateMonthlyStats() {
                     mostOftenCondition = condition;
                 }
             }
-            MONTHS_STATS[i].push(mostOftenCondition);  // Добавляем наиболее частое состояние
+            MONTHS_STATS[i].push(mostOftenCondition);
         }
     }
 
-    findBestWeatherConditions(MONTHS_STATS);
+    find_best_weather_conditions(MONTHS_STATS);
 }
 
-function findBestWeatherConditions(monthsStats) {
+function find_best_weather_conditions(monthsStats) {
     let bestMonthIndex = -1;
     let bestTemperature = -Infinity;
     let bestHumidity = Infinity;
@@ -109,4 +108,4 @@ function findBestWeatherConditions(monthsStats) {
     }
 }
 
-fetchWeatherData();
+fetch_weather_data();
