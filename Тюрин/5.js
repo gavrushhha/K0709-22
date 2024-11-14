d8dbffb9c8315a845981b21c005607eeae69b70d
Artem Tyurin <61313696+rTiRe@users.noreply.github.com>
2024-11-14 12:13:51 +0300

    Add files via upload

--- Тюрин/5.js
+++ Тюрин/5.js
@@ -1,3 +1,18 @@
 async function* paginatedFetchWithStop(url, max_pages) {
-    
+    let page = 1
+    while (page <= max_pages) {
+        try {
+            const response = await fetch(`${url}?page=${page}`)
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`)
+            }
+            const data = await response.json()
+            yield data.results
+            page++
+        }
+        catch (error) {
+            console.error(`Error fetching page ${page}:`, error)
+            break
+        }
+    }
 }
